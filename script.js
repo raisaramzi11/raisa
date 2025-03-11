360ac41d58b0c4b657f2e3b966dbe7abcaf730f2
raisaramzi11 <raisamuriza@students.usu.ac.id>
2025-03-11 22:41:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// Fungsi untuk menampilkan salam berdasarkan waktu
-function setGreeting() {
-    let greetingText = "";
-    let hour = new Date().getHours();
-    if (hour < 12) {
-        greetingText = "Selamat Pagi, Selamat Datang di Perpustakaan Raisa Muriza Ramzi!";
-    } else if (hour < 18) {
-        greetingText = "Selamat Siang, Selamat Datang di Perpustakaan Raisa Muriza Ramzi!";
-    } else {
-        greetingText = "Selamat Malam, Selamat Datang di Perpustakaan Raisa Muriza Ramzi!";
-    }
-    document.getElementById("greeting").innerText = greetingText;
-}
-
 // Fungsi untuk mengaktifkan mode gelap
 document.getElementById("toggle-mode").addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
