2d0c63cd479d7ae0a8685b743a067719858602a5
raisaramzi11 <raisamuriza@students.usu.ac.id>
2025-03-12 14:48:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // Fungsi untuk menampilkan salam selamat datang
 window.alert("Selamat Datang! Selamat menjelajah!");
 
-
+// Fungsi untuk menampilkan slide
+$(docuument).ready(function(){
+$("#flip").click(function(){
+  $("#panel").slideDown("sllow");
+});
+});
