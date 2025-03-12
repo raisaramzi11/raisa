5c456bc60b0a94d6755020b0d61be53e5147124c
raisaramzi11 <raisamuriza@students.usu.ac.id>
2025-03-12 15:01:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 // Fungsi untuk menampilkan salam selamat datang
 window.alert("Selamat Datang! Selamat menjelajah!");
 
-// Fungsi untuk menampilkan slide
-$(docuument).ready(function(){
-$("#flip").click(function(){
-  $("#panel").slideDown("sllow");
-});
-});
+// Fungsi untuk menampilkan animasi mengetik
+  const text = "Halo, selamat datang di website kami!";
+        let index = 0;
+        
+        function typeEffect() {
+            if (index < text.length) {
+                document.getElementById("text").innerHTML += text.charAt(index);
+                index++;
+                setTimeout(typeEffect, 100); // Ubah angka untuk mengatur kecepatan ketikan
+            }
+        }
+
+        typeEffect(); // Memulai animasi
+
+
