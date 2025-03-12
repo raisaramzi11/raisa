e76a0267cad1c080b01df80aa40344ced099d480
raisaramzi11 <raisamuriza@students.usu.ac.id>
2025-03-12 14:32:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 // Fungsi untuk menampilkan salam selamat datang
 window.alert("Selamat Datang! Selamat menjelajah!");
 
-// Fungsi untuk mengaktifkan mode gelap
-document.getElementById("toggle-mode").addEventListener("click", function() {
-    document.body.classList.toggle("dark-mode");
-});
+
